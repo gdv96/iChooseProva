2c83e1e8e0591a465b857b510165d89f0b9af039
Giovanni <giovannidv96@gmail.com>
2021-10-15 19:14:51 +0200

    Aggiunto testo completo della notizia e visualizzazione nel dettaglio della notizia

--- iChooseProva/NewsUIView.swift
+++ iChooseProva/NewsUIView.swift
@@ -12,12 +12,13 @@ struct cardNews: Identifiable{
     var subTitle: String
     var author: String
     var imageName: String
+    var testo: String
     var id = UUID()
 }
 
 var cardsNews = [
-    cardNews(title: "20/07/2021", subTitle: "Mensa anno scolastico 2021/2022, domani al via le iscrizioni al servizio", author: "Domani al via le iscrizioni online al servizio mensa per l’anno scolastico 2021/2022. ", imageName: "mensa"),
-    cardNews(title: "19/07/21", subTitle: "Il 20 luglio farà tappa a Benevento la staffetta “Obiettivo Tricolore” promossa da Alex Zanardi", author: "Domani (20 luglio) farà tappa a Benevento la staffetta “Obiettivo Tricolore”", imageName: "zanardi")
+    cardNews(title: "20/07/2021", subTitle: "Mensa anno scolastico 2021/2022, domani al via le iscrizioni al servizio", author: "Domani al via le iscrizioni online al servizio mensa per l’anno scolastico 2021/2022.", imageName: "mensa", testo:"Domani al via le iscrizioni online al servizio mensa per l’anno scolastico 2021/2022. A renderlo noto è l’assessora all’Istruzione, Rossella Del Prete. A tal proposito l’assessora ricorda che, di recente, la Giunta Comunale ha deliberato l’attivazione delle 'Misure di solidarietà alimentare a favore dei nuclei familiari con figli fruitori del servizio di refezione scolastica'.Pertanto, sino alla soglia ISEE di € 21.000,00 è prevista la contribuzione pari al 100% della tariffa. Oltre la soglia ISEE di € 21.000,00 è prevista la contribuzione pari al 70% della tariffa. I non residenti non potranno ovviamente beneficiare delle suddette contribuzioni."),
+    cardNews(title: "19/07/21", subTitle: "Il 20 luglio farà tappa a Benevento la staffetta “Obiettivo Tricolore” promossa da Alex Zanardi", author: "Domani (20 luglio) farà tappa a Benevento la staffetta “Obiettivo Tricolore”", imageName: "zanardi", testo: "Domani (20 luglio) farà tappa a Benevento la staffetta “Obiettivo Tricolore”, manifestazione che nasce dal progetto di Alex Zanardi di portare in giro per l’Italia un messaggio di speranza, forza e resilienza. Lo rendono noto il sindaco Clemente Mastella e il consigliere delegato allo Sport, Enzo Lauro.")
     
 ]
 
@@ -41,26 +42,29 @@ struct NewsUIView: View {
     var body: some View {
         
             NavigationView{
-                //VStack{
                 ScrollView(.vertical, showsIndicators: false){
                     VStack{
-                        Group{
-                            ForEach(cardsNews) { cardsnews in
+                       Group{
+                            ForEach(cardsNews) {cardsnews in
+                                NavigationLink(destination: DetailView(titleDetail: cardsnews.subTitle, testoCompleto: cardsnews.testo, imageDetail: cardsnews.imageName)) {
+                                    CardView(cardsnews: cardsnews)
+                                }
+                            
                                 /*NavigationLink(destination: DetailUIView(news: news)) {
                                     CardView(news: news)
                                 }*/
-                                ZStack {
-                                    //CardView(news: news)
+                                /*ZStack{
+                                   CardView(cardsnews: cardsnews)
                                    NavigationLink(destination:
                                    PresentationUIView()) {
+                                        //CardView(cardsnews: cardsnews)
                                         EmptyView()
-                                        CardView(cardsnews: cardsnews)
-                                    }
-                                    
-                                }
+                                       
+                                   }
+                                }*/
                             }
                         }.frame(width: 350)
-                            .minimumScaleFactor(0.5)
+                         .minimumScaleFactor(0.5)
                     }
                     Spacer()
                 }.navigationBarTitle("Notizie", displayMode:.automatic)
@@ -72,6 +76,36 @@ struct NewsUIView: View {
 struct NewsUIView_Previews: PreviewProvider {
     static var previews: some View {
         NewsUIView()
+    }
+}
+
+struct DetailView: View {
+    var titleDetail: String
+    var testoCompleto: String
+    var imageDetail: String
+    //var cardsnews: cardNews
+    var body: some View {
+        //Text(cardsnews.testo)
+        ScrollView(.vertical, showsIndicators: false){
+            VStack{
+                Image(imageDetail)
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .cornerRadius(8)
+                    .padding()
+                Text(titleDetail)
+                    .font(.title)
+                    .fontWeight(.black)
+                    .foregroundColor(.primary)
+                    .padding()
+                Text(testoCompleto)
+                    .padding()
+                Spacer()
+                
+            }
+            
+        }
+        
     }
 }
 
@@ -89,14 +123,17 @@ struct CardView: View {
                 Text(cardsnews.title)
                     .font(.headline)
                     .foregroundColor(.gray)
+                    .multilineTextAlignment(.leading)
                 Text(cardsnews.subTitle)
                     .font(.title)
                     .fontWeight(.black)
                     .foregroundColor(.primary)
                     .lineLimit(3)
+                    .multilineTextAlignment(.leading)
                 Text(cardsnews.author)
                     .font(.caption)
                     .foregroundColor(.secondary)
+                    .multilineTextAlignment(.leading)
                
             }
             Spacer()
