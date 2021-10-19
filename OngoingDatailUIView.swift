b1935bb4f091947fdb1f30cb9f2580149ef1bcf8
Giovanni <giovannidv96@gmail.com>
2021-10-19 09:34:07 +0200

    Creazione della view di dettaglio per Ongoing

---
+++ OngoingDatailUIView.swift
@@ -0,0 +1,20 @@
+//
+//  OngoingDatailUIView.swift
+//  iChooseProva
+//
+//  Created by devannogiovanni on 19/10/21.
+//
+
+import SwiftUI
+
+struct OngoingDatailUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct OngoingDatailUIView_Previews: PreviewProvider {
+    static var previews: some View {
+        OngoingDatailUIView()
+    }
+}
